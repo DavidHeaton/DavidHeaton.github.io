13fc2fc8446669a19849272ef4d3a5fc8418a38b
DavidHeaton <arctrect@proton.me>
2025-02-11 08:36:51 +0000

    Changed names of some buttons

--- js/loadSections.js
+++ js/loadSections.js
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded", function() {
     loadSection('../html/racial-traits.html', 'racial-traits-container');
     loadSection('../html/class-features.html', 'class-features-container');
     loadSection('../html/equipment.html', 'equipment-container');
-    loadSection('../html/weapon-attack.html', 'weapon-attack-instructions-container');
+    loadSection('../html/weapon-attack.html', 'weapon-attacks-container');
     loadSection('../html/skill-checks.html', 'skill-checks-container');
     loadSection('../html/attack-tips.html', 'attacking-tips-container');
 });
